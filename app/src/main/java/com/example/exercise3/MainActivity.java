d4b61237f6ca6faa3f9994842c5f0d1a96477bfe
Sonia364 <c0872364@mylambton.ca>
2022-11-30 00:44:13 +0000

    Implemented login functionality

--- app/src/main/java/com/example/exercise3/MainActivity.java
+++ app/src/main/java/com/example/exercise3/MainActivity.java
@@ -1,24 +1,77 @@
 package com.example.exercise3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewGroup root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        root = findViewById(R.id.root);
 
         Switch registerLogin = findViewById(R.id.switchRegister);
+        TextView mainLabel = findViewById(R.id.loginRegisterText);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        EditText confirmPassword = findViewById(R.id.confirmPassword);
+        EditText email = findViewById(R.id.email);
+        Button loginBtn = findViewById(R.id.loginBtn);
+        Button registerBtn = findViewById(R.id.registerBtn);
+
         registerLogin.setOnClickListener(
             v->{
                 if(registerLogin.isChecked()){
-
+                    confirmPassword.setVisibility(View.VISIBLE);
+                    email.setVisibility(View.VISIBLE);
+                    mainLabel.setText("Register");
+                    registerBtn.setVisibility(View.VISIBLE);
+                    loginBtn.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    confirmPassword.setVisibility(View.INVISIBLE);
+                    email.setVisibility(View.INVISIBLE);
+                    mainLabel.setText("Login");
+                    registerBtn.setVisibility(View.INVISIBLE);
+                    loginBtn.setVisibility(View.VISIBLE);
                 }
             }
+        );
+
+        loginBtn.setOnClickListener(
+                v->{
+
+                    String usernameValue = username.getText().toString();
+                    String passwordValue = password.getText().toString();
+                    if(usernameValue.isEmpty() || passwordValue.isEmpty()){
+                        Toast.makeText(this, "Please fill all the fields!", Toast.LENGTH_SHORT).show();
+//                        Snackbar.make(root, "Please fill all the fields!", Snackbar.LENGTH_SHORT)
+//                                .setAction()
+
+                    }else{
+                        if(usernameValue.equals("test") && passwordValue.equals("test")){
+                            Toast.makeText(this, "You are successfully logged in!", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(this, "Username/Password is incorrect!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                }
         );
     }
 }
